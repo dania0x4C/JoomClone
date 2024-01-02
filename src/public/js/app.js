460fb707b52703e042243bcf87d1cd059c19a385
dania0x4C <ljm31881004@gmail.com>
2024-01-02 17:51:07 +0900

    make a showing and saving message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,37 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
 
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+
 socket.addEventListener("open", () => {
   console.log("Connected to server");
 });
 
 socket.addEventListener("message", message => {
-  console.log("I got", message.data, "from server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("unConnected to server");
 });
 
-setTimeout(()=>{
-    socket.send("hello from to browser")
-}, 1000);
+messageForm.addEventListener("submit", function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+});
+
+nickForm.addEventListener("submit", function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send({
+    // 같은 input을 하면 동시에 전부 받아지게 됨 그래서 json형태로 바꾸고 차이를 둘거임
+    type: "nickname",
+    payload: input.value, //이걸 다시 parsing하려면 console에서 json.strungify를 사용해서 찾는게 가능(parse)
+  });
+});
