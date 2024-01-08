6b6d0485482982b8112d46697b684c7256ea5a75
dania0x4C <ljm31881004@gmail.com>
2024-01-08 17:16:29 +0900

    using socketIO

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,37 +1,15 @@
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io(); // back-end의 io와 연결
 
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const nickForm = document.querySelector("#nick");
-
-
-socket.addEventListener("open", () => {
-  console.log("Connected to server");
-});
-
-socket.addEventListener("message", message => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-  console.log("unConnected to server");
-});
-
-messageForm.addEventListener("submit", function handleMessageSubmit(event) {
+welcome = document.querySelector("welcome");
+form = document.querySelector("form");
+function backEndDone(msg){
+  console.log(`back_end say:`, msg)
+}
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(input.value);
-  input.value = "";
-});
-
-nickForm.addEventListener("submit", function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send({
-    // 같은 input을 하면 동시에 전부 받아지게 됨 그래서 json형태로 바꾸고 차이를 둘거임
-    type: "nickname",
-    payload: input.value, //이걸 다시 parsing하려면 console에서 json.strungify를 사용해서 찾는게 가능(parse)
-  });
-});
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, backEndDone); //client에게 event 전달
+  socket.input = "";
+}
+//emit("eventName, payload, function")
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
+import { Socket } from "dgram";
 import express from "express";
-import { WebSocketServer } from "ws";
 import http from "http";
+
+import { Server } from "socket.io";
+
 const app = express();
 
 app.set("view engine", "pug");
@@ -11,23 +14,45 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app); //서버에서 http 사용
+const httpServer = http.createServer(app); //서버에서 http 사용
+//const wss = new WebSocketServer({ server }); // 서버에서 http+webSocket 동시 사용(protocol 두개 사용)
+const wsServer = new Server(httpServer);
 
-const wss = new WebSocketServer({ server }); // 서버에서 http+webSocket 동시 사용(protocol 두개 사용)
-
-function onSocketClose() {
-  console.log("Disconnected from the Browser");
-}
-
-const sockets = [];
-
-wss.on("connection", socket => {
-  sockets.push(socket);
-  console.log("Connected to browser");
-  socket.on("close", onSocketClose);
-  socket.on("message", message => {
-    sockets.forEach(aSocket => aSocket.send(message.toString("utf8")));
+wsServer.on("connection", socket => {
+  socket.on("enter_room", (roomName, done) => {// 이 방식은 함수를 front에서 실행시킴 그렇게 해서 보안 문제를 해결
+    // 간단하게 emit한 정보를 받아올 수 있음
+    console.log(roomName);
+    setTimeout(() => {
+      done("TimeOut!");
+    }, 10000);
   });
-}); //wss연결되었을때 작동하는 함수
+});
 
-server.listen(3000, handleListen);
+
+// function onSocketClose() {
+// console.log("Disconnected from the Browser");
+// }
+//on(eventName, (payload, function))// 중간에 추가 가능   
+
+// const sockets = [];
+// wss.on("connection", socket => {//socket.on은 call
+//   sockets.push(socket);//프론트로 data Push
+//   socket["nickname"] = "Anon"; // socket의 key값을 []로 지정
+//   console.log("Connected to browser");
+//   socket.on("close", onSocketClose);// 연결을 끊어주기
+//   socket.on("message", message => {
+//     const parsed = json.parse(message); //여기가 이해가 안 됨 parse의 역할
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach(aSocket =>
+//           aSocket.send(
+//             `${socket.nickname}: ${message.payload.toString("utf8")}` // 다른 브라우저에 message 보내기
+//           )
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload; //socket에 정보를 추가하는 방법
+//     }
+//   });
+// }); //wss연결되었을때 작동하는 함수
+
+httpServer.listen(3000, handleListen);
